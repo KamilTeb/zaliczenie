50d238fb69702aafe1bd2ccfcece17872fe00168
Kamil Trybel <ktrybel@teb-gdansk.ad>
2024-04-13 09:51:02 +0200

    Change directories and js code.

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ const btn = document.querySelector('button');
 
 btn.addEventListener('click', () => {
     alert('Przycisk został kliknięty - JS działa poprawnie.')
+    document.body.style.backgroundColor = 'blue';
 })
